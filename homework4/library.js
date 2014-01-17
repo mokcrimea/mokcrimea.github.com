306b3e99fd18c0bc05904c56662780f0d11a2719
Valentyn Khatsaiuk <mok.crimea@gmail.com>
2014-01-17 08:10:25 +0000

    попытка спроектировать модуль

--- homework4/library.js
+++ homework4/library.js
@@ -1,57 +1,49 @@
-var testString = "КОРАБЛИ, лавировали, лавировали. Да не вылавировали ёжик Ёжик";
-var vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
-var consonants = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'];
+  var testString = "КОРАБЛИ, лавировали, лавировали. Да не вылавировали ёжик Ёжик";
 
-function getLetters(inputText, Letters) {
-  var outputLetters = '';
-  for (var i = 0; i < inputText.length; i++) {
-    for (var j = 0; j < Letters.length; j++) {
-      if (inputText[i].toLowerCase() == Letters[j]) {
-        outputLetters += inputText[i] + ', ';
-      }
-    }
-  }
-
-  return (outputLetters.substr(0, outputLetters.length - 2));
-}
-
-function WordLb(Text) {
-  this.count = function() {
+  var WordsLibrary = (function() {
+    var vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+    var consonants = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'];
+    var outputLetters = ''; // для функции getLetter
     var regexp = /[а-я]/i;
     var k = 0;
-    for (var i = 0; i < Text.length; i++) {
-      if (regexp.test(Text[i])) {
-        if (!regexp.test(Text[i - 1])) {
-          k++;
+    var input = '';
+
+      function getLetters(inputText, Letters) {
+        var outputLetters = '';
+        for (var i = 0; i < inputText.length; i++) {
+          for (var j = 0; j < Letters.length; j++) {
+            if (inputText[i].toLowerCase() == Letters[j]) {
+              outputLetters += inputText[i] + ', ';
+            }
+          }
         }
+
+        return (outputLetters.substr(0, outputLetters.length - 2));
       }
-    }
-    return k;
-  };
 
-  this.vowels = function() {
-    return (getLetters(Text, vowels));
-  };
 
-  this.consonants = function() {
-    return (getLetters(Text, consonants));
-  };
+    return {
+      count: function() {
+        k = 0; // обнуляем объявленую ранее переменную
+        for (var i = 0; i < Text.length; i++) {
+          if (regexp.test(Text[i])) {
+            if (!regexp.test(Text[i - 1])) {
+              k++;
+            }
+          }
+        }
+        return k;
+      },
 
-  // this.syllables = function() {
-  //   var regexp = /[а-я]/i;
-  //   var tempSyllable = '';
-  //   var tempArray = [];
-  //   for (var i = 0; i < Text.length; i++) {
-  //     for (var j = 0; i < consonants.length; j++) {
-  //       if (Text[i] == consonants[j]) {
-  //         tempSyllable += Text[i];
-  //       }
-  //     }
-  //   }
-  // };
-}
+      vowels: function() {
+        return (getLetters(Text, vowels));
+      },
 
-var Test = new WordLb(testString);
-console.log(Test.count()); // количество слов
-console.log(Test.vowels()); // вывод всех гласных
-console.log(Test.consonants()); // вывод всех согласных
+      consonants: function() {
+        return (getLetters(Text, consonants));
+      }
+    };
+
+  })();
+
+  console.log(WordsLibrary.count(testString));
