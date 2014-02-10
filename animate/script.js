e90358179d519d92efa44e9f4c2dbbbd16efd3c9
Valentyn Khatsaiuk <mok.crimea@gmail.com>
2014-02-10 23:00:45 +0000

    изменил поведение progress bar-a

--- animate/script.js
+++ animate/script.js
@@ -1,20 +1,14 @@
-document.body.getElementsByTagName('input')[0].addEventListener('keyup', progressBar);
+document.body.getElementsByTagName('button')[0].addEventListener('click', progressBar);
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function progressBar() {
-    var maxWidth = document.body.getElementsByClassName('progress_bar')[0].clientWidth;
-    var currentWidth = document.body.getElementsByClassName('bar')[0].style;
-    var percents = document.body.getElementsByClassName('percents')[0];
-    var step = maxWidth / 100;
-    var count = document.body.getElementsByClassName('percents')[0].textContent.replace(/\%/, '');
-    var input = document.body.getElementsByTagName('input')[0].value;
-
-    if (+input > 100) {
-        document.body.getElementsByTagName('input')[0].value = 100;
-        input = 100;
-    } else if (+input < 0) {
-        document.body.getElementsByTagName('input')[0].value = 0;
-        input = 0;
-    }
+    var input = getRandomInt(1, 100),
+        count = document.body.getElementsByClassName('percents')[0].textContent.replace(/\%/, ''),
+        time = 2000 / Math.abs(input - count);
+    document.body.getElementsByClassName('bar')[0].style.width = 210 / 100 * input + 'px';
 
     if (+input > +count) {
         changeWidth(true);
@@ -29,12 +23,11 @@ function progressBar() {
             } else {
                 count--;
             }
-            currentWidth.width = count * step + 'px';
-            percents.textContent = count + '%';
+            document.body.getElementsByClassName('percents')[0].textContent = count + '%';
 
             setTimeout(function() {
                 changeWidth(bool);
-            }, 50);
+            }, time);
         }
     }
 }
