488ed5f80f08b115dc06cdd57be151c1be4939fa
Valentyn Khatsaiuk <mok.crimea@gmail.com>
2014-02-12 23:51:25 +0000

    изменения в логике progress bara, исправлена ошибка с кнопкой

--- animate/script.js
+++ animate/script.js
@@ -1,33 +1,17 @@
-document.body.getElementsByTagName('button')[0].addEventListener('click', progressBar);
+function animateProgress(percent) {
+    var p = document.getElementsByClassName('percents')[0].innerHTML.replace(/\%/, '');
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function progressBar() {
-    var input = getRandomInt(1, 100),
-        count = document.body.getElementsByClassName('percents')[0].textContent.replace(/\%/, ''),
-        time = 2000 / Math.abs(input - count);
-    document.body.getElementsByClassName('bar')[0].style.width = 210 / 100 * input + 'px';
-
-    if (+input > +count) {
-        changeWidth(true);
-    } else if (+input < +count) {
-        changeWidth(false);
-    }
-
-    function changeWidth(bool) {
-        if (+count != +input) {
-            if (bool) {
-                count++;
-            } else {
-                count--;
-            }
-            document.body.getElementsByClassName('percents')[0].textContent = count + '%';
-
-            setTimeout(function() {
-                changeWidth(bool);
-            }, time);
-        }
+    if (p < percent) {
+        p++;
+        document.getElementsByClassName('percents')[0].innerHTML = p + '%';
+        document.getElementsByClassName('bar')[0].style.width = p + '%';
     }
 }
+
+bar = function startProgress(percent, timeout) {
+    timeout = typeof timeout !== 'undefined' ? timeout : 150;
+    window.setInterval('animateProgress(' + percent + ')', timeout);
+    return true;
+};
+
+bar(20);
